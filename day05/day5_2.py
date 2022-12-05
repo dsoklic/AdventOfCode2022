c5693b69fbe6663dd03e2a06c941d9f1e305498a
Domen Soklic <soklicd@gmail.com>
2022-12-05 16:52:37 +0100

    Day 5

---
+++ day05/day5_2.py
@@ -0,0 +1,28 @@
+import re
+
+f = open("day05/input.txt")
+
+field, instructions = f.read().split('\n\n')
+field = field.split('\n')
+
+stack_num = len(re.findall('(\d+)', field[-1]))
+stacks = [ [] for _ in range(stack_num) ]
+
+field.reverse()
+
+for line in field[1:]:
+    for i in range(stack_num):
+        container = line[i*4 + 1].strip()
+
+        if container:
+            stacks[i].append(container)
+
+instructions = re.findall('move (\d+) from (\d+) to (\d+)', instructions)
+
+for instruction in instructions:
+    amount, a, b = [int(x) for x in instruction]
+    
+    stacks[b-1] += stacks[a-1][-amount:]
+    stacks[a-1] = stacks[a-1][:-amount]
+
+print(''.join([x.pop() for x in stacks]))
