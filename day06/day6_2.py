b27d3f9f08f6801e8d024e15c174a4ff034c44b4
Domen Soklic <domen.soklic@cosylab.com>
2022-12-06 10:30:46 +0100

    Day 6

---
+++ day06/day6_2.py
@@ -0,0 +1,10 @@
+input = open("day06/input.txt").readline()
+
+window_size = 14
+
+for i in range(len(input)-window_size+1):
+  window = input[i:i+window_size]
+
+  if len(window) == len(set(window)):
+    print(i+window_size)
+    break
