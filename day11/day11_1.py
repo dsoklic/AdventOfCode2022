ce9f51ce620f6ef3c21c0ffe49af5bbffc6888bf
Domen Soklic <soklicd@gmail.com>
2022-12-11 10:09:29 +0100

    Day 11 part 1

---
+++ day11/day11_1.py
@@ -0,0 +1,65 @@
+import re
+
+f = open("day11/input.txt")
+
+data = f.read().split('\n\n')
+
+monkies = []
+
+def inspect(item, operation) -> int:
+    operator, amount = operation
+
+    if amount == 'old':
+        amount = item
+    else:
+        amount = int(amount)
+
+    match operator:
+        case '+':
+            item += amount
+        case '-':
+            item -= amount
+        case '/':
+            item /= amount
+        case '*':
+            item *= amount
+
+    return int(item)
+
+for monkey in data:
+    monkey = monkey.split('\n')
+    # id = int(re.findall('Monkey (\d+)', monkey[0])[0])
+    items = [int(x) for x in re.findall('(\d+)', monkey[1])]
+    operation = re.findall('Operation: new = old ([+\-\*/]) (\d+|old)', monkey[2])[0]
+    test_divisibility = int(re.findall('Test: divisible by (\d+)', monkey[3])[0])
+    true_monkey = int(re.findall('If true: throw to monkey (\d+)', monkey[4])[0])
+    false_monkey = int(re.findall('If false: throw to monkey (\d+)', monkey[5])[0])
+
+    monkies.append({
+        'items': items,
+        'operation': operation,
+        'test_divisibility': test_divisibility,
+        'true_monkey': true_monkey,
+        'false_monkey': false_monkey,
+        'inspections': 0
+    })
+
+for _ in range(20):
+    for monkey in monkies:
+        for item in monkey['items']:
+            monkey['inspections'] += 1
+
+            item = inspect(item, monkey['operation'])
+            item /= 3 # lose interest
+            item = int(item)
+
+            # Is divisible?
+            if item % monkey['test_divisibility'] == 0:
+                monkies[monkey['true_monkey']]['items'].append(item)
+            else:
+                monkies[monkey['false_monkey']]['items'].append(item)
+
+        monkey['items'] = [] # threw everything away
+
+activity_list = sorted([x['inspections'] for x in monkies], reverse=True)
+print(activity_list[0] * activity_list[1])
