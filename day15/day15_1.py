de47071ee464dbc6efc8fd14bd9a9cd171ef38a8
Domen Soklic <soklicd@gmail.com>
2022-12-15 15:52:02 +0100

    Day 15 part 1

--- day15/day15_1.py
+++ day15/day15_1.py
@@ -13,13 +13,15 @@ def manhattan_distance(x1, y1, x2, y2):
 f = open("day15/input.txt")
 
 points = []
-
+beacons = set()
 limits = {}
 
 for line in f.readlines():
   matches = re.findall("Sensor at x=([\-\d]+), y=([\-\d]+): closest beacon is at x=([\-\d]+), y=([\-\d]+)", line)[0]
   x1, y1, x2, y2 = [int(foo) for foo in matches]
   dist = manhattan_distance(x1, y1, x2, y2)
+
+  beacons.add((x2, y2))
 
   if limits == {}:
     limits = {'left':x1,'right':x1,'up':y1,'down':y1}
@@ -32,8 +34,11 @@ for line in f.readlines():
   points.append(Point(x1, y1, dist))
 
 locations = 0
-analysed_y = 10
+analysed_y = 2000000
 for analysed_x in range(limits['left'], limits['right'] + 1):
+  if (analysed_x, analysed_y) in beacons:
+    continue
+
   for point in points:
     if manhattan_distance(analysed_x, analysed_y, point.x, point.y) <= point.radius:
       locations += 1
